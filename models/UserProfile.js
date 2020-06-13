0663c9edf26e60363f8b9139d5faf9478f6eaadf
Dominik Hruza <dominikhruza92@gmail.com>
2020-06-13 17:17:05 +0200

    refactored user profile api to stored procedures

--- models/UserProfile.js
+++ models/UserProfile.js
@@ -33,22 +33,12 @@ module.exports = class UserProfile {
 
   async getUserPhotos() {
     try {
-      const response = await db.query(
-        `SELECT username,
-                photos.id as photoId, 
-                users.id as userId,
-                (SELECT COUNT(likes.photo_id) FROM likes WHERE photo_id = photoId) AS likesNum       
-        FROM photos
-        INNER JOIN users
-        ON photos.user_id = users.id
-        WHERE photos.user_id = ?;`,
-        [this.id]
-      );
+      const response = await db.query(`CALL getUserPhotos(?)`, [this.id]);
 
       //Set user photos prop based on query result
       const data = response[0];
-      this.userName = data[0].username;
-      this.photos = data;
+      this.userName = data[0][0].username;
+      this.photos = data[0];
     } catch (error) {
       console.error(error);
     }
@@ -57,15 +47,7 @@ module.exports = class UserProfile {
   async getComments() {
     for (let index = 0; index < this.photos.length; index++) {
       const photo = this.photos[index];
-      const result = await db.query(
-        `SELECT username, comment_text, comments.created_at FROM comments
-         INNER JOIN users
-         ON comments.user_id = users.id
-         INNER JOIN photos
-         ON comments.photo_id = photos.id
-         WHERE photo_id = ?;`,
-        [photo.photoId]
-      );
+      const result = await db.query(`CALL getComments(?);`, [photo.photoId]);
 
       photo.comments = result[0];
     }
@@ -73,20 +55,10 @@ module.exports = class UserProfile {
 
   async getFollowers() {
     try {
-      const responseCount = await db.query(
-        `SELECT COUNT(followee_id) AS count FROM follows WHERE followee_id = ?;`,
-        [this.id]
-      );
-      const responseList = await db.query(
-        `SELECT users.id, users.username FROM follows 
-        INNER JOIN users
-        ON follows.follower_id = users.id
-        WHERE followee_id = ?;`,
-        [this.id]
-      );
+      const response = await db.query(`CALL getFollowers(?)`, [this.id]);
 
-      const followerCount = responseCount[0][0];
-      const followerList = responseList[0];
+      const followerCount = response[0][0][0];
+      const followerList = response[0][1];
 
       this.followers = followerCount;
       this.followers.list = followerList;
@@ -97,23 +69,14 @@ module.exports = class UserProfile {
 
   async getFollows() {
     try {
-      const responseCount = await db.query(
-        `SELECT count(followee_id) AS count FROM follows WHERE follower_id = ?;`,
-        [this.id]
-      );
-      const responseList = await db.query(
-        `SELECT users.id, users.username FROM follows 
-        INNER JOIN users
-        ON follows.followee_id = users.id
-        WHERE follower_id = ?;`,
-        [this.id]
-      );
+      const response = await db.query(`CALL getFollows(?);`, [this.id]);
 
-      const followsCount = responseCount[0][0];
-      const followsList = responseList[0];
+      const followsCount = response[0][0][0];
+      const followsList = response[0][1];
 
       this.follows = followsCount;
       this.follows.list = followsList;
+      
     } catch (error) {
       console.error(error);
     }
